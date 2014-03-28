f84f78a50d0630c415c47a5bbf17cf4948c30223
Andrew Krause <a.c.krause@gmail.com>
2014-03-28 15:51:32 -0700

    Added .extend(), which adds key-value pairs to a given object

--- src/underbar.js
+++ src/underbar.js
@@ -232,7 +232,7 @@ var _ = { };
     if (iterator === undefined) {
       iterator = _.identity
     }
-    
+
     var invertedIterator = function(value) {
       return !iterator(value);
     }
@@ -260,6 +260,13 @@ var _ = { };
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    for (var i = 0; i < arguments.length; i++) {
+      _.each(arguments[i], function(val, key) {
+        obj[key] = val;
+      });
+    }
+
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
